d30a24e6f62d12ed2cf00cfde070b6c1f6ac027d
Leandro Cruz <leandro.programmer@hotmail.com>
2025-05-04 18:12:03 -0300

    feat (Header): Adding styles in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,14 @@ import { MenuIcon } from '~/components/icons/menu-icon'
 export function Header() {
   return (
     <header className="flex items-center justify-between p-4">
-      <div className="left">
+      <div className="Left gap-12">
         <Image src={logoColored} alt="Logo colored" width={103} height={35} />
       </div>
 
-      <div className="buttons-group">
-        <MenuIcon height={24} width={24} />
+      <div className="Buttons-group gap-4">
+        <div className="relative -right-1 rounded-full p-1 transition duration-200 hover:cursor-pointer hover:bg-gray-100">
+          <MenuIcon height={24} width={24} />
+        </div>
       </div>
     </header>
   )
